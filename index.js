9ccc2e166074e35d897076749c90c85679801dd5
Ryan <ryanwbachman@gmail.com>
2017-10-08 23:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({[item]: Math.floor(Math.random()*100)});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if(!cart.length){
+    console.log("Your shopping cart is empty.");
+  } else {
+    var stringArray = []; var item = ""; var cost = "";
+    for(var i=0; i<cart.length; i++){
+      item = Object.keys(cart[i]);
+      cost = cart[i][item];
+      stringArray.push(`${item} at $${cost}`);
+    }
+    if(cart.length==1){
+      console.log(`In your cart, you have ${stringArray[0]}.`);
+    } else if(cart.length==2){
+      console.log(`In your cart, you have ${stringArray[0]} and ${stringArray[1]}.`);
+    } else {
+      stringArray[stringArray.length-1] = `and ${stringArray[stringArray.length-1]}`;
+      console.log(`In your cart, you have ${stringArray.join(", ")}.`);
+    }
+  }
 }
 
 function total() {
